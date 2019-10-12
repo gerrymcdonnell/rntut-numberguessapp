40e98609163f75899f0f883c7479a2b088f68cfc
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2019-10-12 21:34:56 +0100

    fixed header

--- App.js
+++ App.js
@@ -19,8 +19,6 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
