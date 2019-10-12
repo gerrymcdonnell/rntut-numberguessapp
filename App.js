45794614c46650a9bc9b85c61094aedaa58ea8f1
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2019-10-12 21:31:39 +0100

    created header componet

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header'
+
+/**
+ * yt video
+ * https://www.youtube.com/watch?v=qSRrxpdMpVc&t=2021s
+ * 3:48:39 starts
+ */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Header title="Guess a number"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const Header=props=>{};
+/**
+ * functional component
+ * returns a view
+ */
+const Header=props=>{
+    return(
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>
+                {props.title}
+            </Text>
+        </View>
+    )
+};
 
-const styles=StyleSheet.create({});
+const styles=StyleSheet.create({
+    header:{
+        width:'100%',
+        height:90,
+        paddingTop:36,
+        backgroundColor:'#f7287b',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    headerTitle:{
+        color:'black',
+        fontSize:18
+    }
+});
 
 export default Header;
