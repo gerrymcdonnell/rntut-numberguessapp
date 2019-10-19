e89353d68e03f44e5af71b3d57bac4846d1eb64c
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2019-10-19 15:13:07 +0100

    fixed error with view

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, TextInput,View,Button } from 'react-native';
 
+import Card from '../components/Card'
+
 const StartGameScreen=props=>{
     return(
         <View style={styles.screen}>
             
             <Text style={styles.title}>Start a New Game!</Text>
 
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
 
                 <Text>Select a Number</Text>
-                <TextInput/>
                 <TextInput/>
                 <View style={styles.buttonContainer}>
                     <Button title="Reset" onPress={()=>{}} />
                     <Button title="Confirm" onPress={()=>{}} />
                 </View>
-
-            </View>
-
+                
+            </Card>
         </View>
     )
 };
@@ -38,15 +38,7 @@ const styles=StyleSheet.create({
     inputContainer:{
         width:300,
         maxWidth:'80%',
-        alignItems:'center',
-        shadowColor:'black',
-        shadowOffset:{width:0,height:2},
-        shadowRadius:6,
-        shadowOpacity:0.26,
-        elevation:5,
-        backgroundColor:'white',
-        padding:20,
-        borderRadius:10
+        alignItems:'center'        
     },
     title:{
         fontSize:20,
