6dc17849403e6ae62de62d313be6d4fbce8f75e4
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2019-10-19 21:08:49 +0100

    added game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,8 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+
+import NumberContainer from '../components/numberContainer';
+import Card from '../components/Card';
 
 const generateRandomBetween=(min,max,exclude)=>{
     min=Math.ceil(min);
@@ -19,13 +22,29 @@ const GameScreen=props=>{
     //react hooks
     const[currentGuess,setCurrentGuess]=useState(generateRandomBetween(1,100,props.userChoice));
 
-    return(
-        //put component stuff here
-        <View>
-
+    return(        
+        <View style={styles.screen}>
+            <Text>CPU Guess'ed</Text>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title="Lower" onPress={()=>{}} />
+                <Button title="Greater" onPress={()=>{}} />
+            </Card>
         </View>
     )
 };
-const styles=StyleSheet.create({});
+const styles=StyleSheet.create({
+    screen:{
+        flex:1,
+        padding:10,
+        alignItems:'center'
+    },
+    buttonContainer:{
+        flexDirection:'row',
+        justifyContent:'space-around',
+        marginTop:20,
+        maxWidth:'80%'
+    }
+});
 
 export default GameScreen;
